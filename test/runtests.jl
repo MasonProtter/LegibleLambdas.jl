6c11501a51acf83ad2be609597f5953f3a1a3817
MasonProtter <mason.protter@icloud.com>
2021-01-08 12:05:18 -0700

    don't specialcase closure testing on 1.0

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,11 +20,7 @@ end
 @testset "Closures" begin
     D(f, ϵ=1e-10) = @λ(x -> (f(x+ϵ)-f(x))/ϵ)
 
-    if VERSION < v"1.1.0"
-        @test repr(D(sin)) == "(x -> (f(x + ϵ) - f(x)) / ϵ)"
-    else
-        @test repr(D(sin)) == "(x -> ((sin)(x + 1.0e-10) - (sin)(x)) / 1.0e-10)"
-    end
+    @test repr(D(sin)) == "(x -> ((sin)(x + 1.0e-10) - (sin)(x)) / 1.0e-10)"
 
     @test D(sin)(π) == -1.000000082740371
 end
